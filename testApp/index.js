3f2d180cefd565de542e5d783e3863049ed377ea
RafaelVillagomezDev <rafaelvillagomez.dev@gmail.com>
2022-05-18 18:23:15 +0200

    test 1,2,3,4 class

--- testApp/index.js
+++ testApp/index.js
@@ -5,6 +5,37 @@ class Room {
     this.rate = rate
     this.discounts = discounts
   }
+
+  isOccupied(currentDate) {
+    const date_entry = this.bookings[0].checkIn
+    const date_exit = this.bookings[0].checkOut
+
+    if (currentDate >= date_entry && currentDate <= date_exit) {
+      return false
+    } else {
+      return this.bookings[0]
+    }
+  }
+
+  availableRooms(startDate, endDate) {
+    //TENEMOS AS FECHAS DEL INTERVALO QUE E SIGUAL AL TOTAL , A ESTE NUMERO ES NUESTRO DENOMINADOR .El NUMERADOR SERIA LA RESTA ENTRE EL STARTDATE
+    //QUE BUSCAMOS Y LA FECHA DE FIN DE LA RESERVA
+    if (
+      this.bookings[0].checkOut > startDate &&
+      this.bookings[0].checkOut <= endDate
+    ) {
+      const diasFechaIntervaloTotal =
+        new Date(endDate).getDate() - new Date(startDate).getDate()
+      const diasOcupacionIntervalo =
+        new Date(endDate).getDate() -
+        new Date(this.bookings[0].checkOut).getDate()
+      const percertageInterval =
+        parseFloat(diasOcupacionIntervalo / diasFechaIntervaloTotal) * 100
+      return percertageInterval
+    } else {
+      return false
+    }
+  }
 }
 
 class Booking {
@@ -36,17 +67,44 @@ const copiaBooking = {
   room: 122,
 }
 
-function isOccupied(room, currentDate) {
-  const date_entry = room.bookings.checkIn
-  const date_exit = room.bookings.checkOut
+const booking = new Booking({ ...copiaBooking })
+const room = new Room({ ...copiaRoom, bookings: booking })
 
-  if (currentDate >= date_entry && currentDate <= date_exit) {
-    return room.bookings
-  } else {
-    return null
+//HACEMOS EL FOR POR QUE NOS RRECORREMOS UN ARRAY DE HABITACIONES DEPENDIENDO DE CUANTAS METO
+function totalOccupancyPercentage(room, startDate, endDate) {
+  room.bookings = [booking]
+  const totalBookings = Object.keys(booking)
+  for (let i = 0; i < totalBookings.length; i++) {
+    if (booking.checkOut > startDate && booking.checkOut <= endDate) {
+      const avalaibleRoom = Object.keys(booking)
+      const percentage = (avalaibleRoom.length / totalBookings.length) * 100
+      return percentage
+    } else {
+      return false
+    }
   }
 }
 
-//   isOccupied(room,new Date('28 May 2022 14:48 UTC').toISOString())
+function availableRooms(rooms, startDate, endDate) {
+  rooms.bookings = [booking]
+  const totalBookings = Object.keys(booking)
+  for (let i = 0; i < totalBookings.length; i++) {
+    if (
+      rooms[i].booking.checkOut > startDate &&
+      rooms[i].booking.checkOut <= endDate
+    ) {
+      return rooms
+    } else {
+      return false
+    }
+  }
+}
 
-module.exports = { Room, Booking, copiaBooking, copiaRoom, isOccupied }
+module.exports = {
+  Room,
+  Booking,
+  copiaBooking,
+  copiaRoom,
+  totalOccupancyPercentage,
+  availableRooms,
+}
--- testApp/index.test.js
+++ testApp/index.test.js
@@ -3,20 +3,44 @@ const {
   copiaRoom,
   copiaBooking,
   Booking,
-  isOccupied,
+  totalOccupancyPercentage,
 } = require('./index')
 
 describe('Test Rooms and Bookings', () => {
   const room = new Room({ ...copiaRoom, name: 'pepe' })
-  test('validando name devuelve name ', () => {
+  test('validando name devuelve guest name ', () => {
     expect(room.name).toBe('pepe')
   }),
-    test('habitacion ocupada devuelve guest (room.bookings)', () => {
+    test('habitacion si esta ocupada devuelve guest (room.bookings), si no false', () => {
+      const booking = new Booking({ ...copiaBooking })
+      const room = new Room({ ...copiaRoom, bookings: booking })
+      room.bookings = [booking]
+      expect(
+        room.isOccupied(new Date('22 May 2022 14:48 UTC').toISOString()),
+      ).toBe(false)
+    }),
+    test('retorna todas las habitaciones que no estan ocupadas en una duracion , el valor a esperar es el porcentaje que creemos q nos dara', () => {
+      const booking = new Booking({ ...copiaBooking })
+      const room = new Room({ ...copiaRoom, bookings: booking })
+      room.bookings = [booking]
+      expect(
+        room.availableRooms(
+          new Date('20 May 2022 14:48 UTC').toISOString(),
+          new Date('25 May 2022 14:48 UTC').toISOString(),
+        ),
+      ).toEqual(20)
+    }),
+    test('retorna todas las habitacion que hay en un intervalo de fechas. Retorna porcentaje y espera porcentaje', () => {
       const booking = new Booking({ ...copiaBooking })
       const room = new Room({ ...copiaRoom, bookings: booking })
 
       expect(
-        isOccupied(room, new Date('20 May 2022 14:48 UTC').toISOString()),
-      ).toBe(room.bookings)
-    })
+        totalOccupancyPercentage(
+          room,
+          new Date('20 May 2022 14:48 UTC').toISOString(),
+          new Date('25 May 2022 14:48 UTC').toISOString(),
+        ),
+      ).toBe(100)
+    }),
+    test('Retorna todas las habitaciones que no estan ocupadas en ese rango', () => {})
 })
